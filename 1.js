9b997ecddc2eda17df9ac9e4592f5812bc242a59
SaraswatiKumari1 <90024449+SaraswatiKumari1@users.noreply.github.com>
2021-11-01 15:19:40 +0530

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,9 @@
+var name1 = "Saraswati Kumari";
+var roll_no = 2006091;
+var branch = "CSE";
+console.log("I am " + name1 + " and my roll no is " + roll_no);
+console.log("My branch is " + branch);
+var a = 6;
+var b = 5;
+var c = a*b;
+console.log("Multiplication of " + a + " and "+ b + " = "+ c);
