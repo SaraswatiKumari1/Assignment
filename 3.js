31d2f1379b5f374fa0e4e78a73c82b4da5981f37
SaraswatiKumari1 <90024449+SaraswatiKumari1@users.noreply.github.com>
2021-11-01 16:06:42 +0530

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,15 @@
+console.log("Example of UNDEFINED");
+var a;
+console.log(a);
+if (a == undefined)
+    console.log("Variable a is undefined");
+else
+    console.log("Variable a is defined and its value = " + a);
+console.log("Example of DEFINED");
+var b = 7;
+if (b == undefined)
+    console.log("Variable b is undefined");
+else
+    console.log("Variable b is defined and its value = " + b);
+console.log("Example of NOTDEFINED");
+console.log(c);
